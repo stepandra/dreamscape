dcb3b1897150ff413d7034cd1b669d0b71fd741e
Stepan Mytchyk <stepandra94@gmail.ckm>
2016-03-03 19:29:23 +0200

    Initial commit
    Recover some old code for dreamscape test task.

---
+++ Source/main.js
@@ -0,0 +1,24 @@
+require("babel-polyfill");
+import 'es6-promise';
+import {drawTree} from './tree';
+
+let arr = [];
+const socket = io();
+
+let createView = (node) => {
+  arr.push(node);
+  Array.isArray(node.children) && node.children.map(child => createView(child))
+}
+
+let updateView = (result, left, right) => {
+  [result[left].name, result[right].name] = [result[right].name, result[left].name];
+  return result;
+}
+
+socket.on('notification', (treeData) => {
+  createView(treeData);
+drawTree(treeData);
+let updated_data = updateView(arr, 0, 1);
+
+socket.emit('update', updated_data);
+});
